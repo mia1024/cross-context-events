6b96100d08d74a60811e1dab1799fc3833933516
Mia Wang <m@mia.wang>
2021-07-22 22:52:14 -0700

    First npm publish

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+module.exports = {
+    mode: "production",
+    target: "node",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+                options: {
+                    compilerOptions: {
+                        declaration: false
+                    },
+                }
+            },
+        ]
+    },
+    entry: "./src/index.ts",
+    output: {
+        clean: true,
+        filename: "cross-context-events.min.js",
+        path: path.resolve(__dirname, "dist"),
+        library: {
+            name: "cross-context-events",
+            type: "umd"
+        }
+    },
+    resolve: {
+        extensions: [".ts"],
+    },
+    stats: {
+        errorDetails: true
+    },
+
+}
